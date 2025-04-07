741ee004af40b743a490f7177c92e40dd5a386ce
MATTHEWMURPHY-STUDEN <g00430349@atu.ie>
2025-04-07 12:13:16 +0100

    first name  Junit Test

--- src/main/java/org/example/testPassenger.java
+++ src/main/java/org/example/testPassenger.java
@@ -24,6 +24,12 @@ public class testPassenger {
 
         assertEquals(expected:Attendee.Title());
     }
+    @Test
+    void Title(){
+
+
+        assertEquals(expected:Attendee.Fname());
+    }
 
     @AfterEach
     void tearThis(){
